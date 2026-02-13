1f7b8014eebf9bf95e574dcfed20b91d3f01cee5
Viren Mirpuri <Viren389@gmail.com>
2026-02-13 15:51:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7667047704-c8ebf",
+  "appId": "1:877468884327:web:3669786149ba253f4e23c2",
+  "apiKey": "AIzaSyAso5dYsoDcOpo8PKPthO0ssMWhG_tyOLk",
+  "authDomain": "studio-7667047704-c8ebf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "877468884327"
+};
