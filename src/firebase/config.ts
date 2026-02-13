ad9557ace21889ce0da6d43d43ae08214be35859
Viren Mirpuri <Viren389@gmail.com>
2026-02-13 18:34:34 +0000

    Fix this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:877468884327:web:3669786149ba253f4e23c2",
   "apiKey": "AIzaSyAso5dYsoDcOpo8PKPthO0ssMWhG_tyOLk",
   "authDomain": "studio-7667047704-c8ebf.firebaseapp.com",
+  "storageBucket": "studio-7667047704-c8ebf.appspot.com",
   "measurementId": "",
   "messagingSenderId": "877468884327"
 };
