e440b9038dfe7e665f2e83801a87ff10c9c06d95
Viren Mirpuri <Viren389@gmail.com>
2026-02-13 21:34:29 +0000

    # Firebase Configuration - get these from your project settings NEXT_PUB

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7667047704-c8ebf",
-  "appId": "1:877468884327:web:3669786149ba253f4e23c2",
   "apiKey": "AIzaSyAso5dYsoDcOpo8PKPthO0ssMWhG_tyOLk",
   "authDomain": "studio-7667047704-c8ebf.firebaseapp.com",
-  "storageBucket": "studio-7667047704-c8ebf.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "877468884327"
+  "projectId": "studio-7667047704-c8ebf",
+  "storageBucket": "studio-7667047704-c8ebf.firebasestorage.app",
+  "messagingSenderId": "877468884327",
+  "appId": "1:877468884327:web:3669786149ba253f4e23c2"
 };
