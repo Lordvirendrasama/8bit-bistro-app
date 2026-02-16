332e1e1dd8cd126ee42e431f74246d58bb01f91e
Viren Mirpuri <Viren389@gmail.com>
2026-02-16 21:38:49 +0000

    Zoom the logo much more.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,9 +6,9 @@ import Logo from "@/components/Logo";
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-40 items-center justify-center">
+      <div className="container mx-auto flex h-48 items-center justify-center">
         <Link href="/dashboard">
-          <Logo className="h-32" />
+          <Logo className="h-40" />
         </Link>
       </div>
     </header>
