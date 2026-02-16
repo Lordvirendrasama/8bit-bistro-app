29b5f82acb38bf780692f4ab56e2672551ec99a3
Viren Mirpuri <Viren389@gmail.com>
2026-02-16 21:36:42 +0000

    I want this to be the main logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 export default function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="/THE 8 BIT BISTRO.png"
+      src="/THE%208%20BIT%20BISTRO.png"
       alt="The 8Bit Bistro Logo"
       width={1000}
       height={250}
